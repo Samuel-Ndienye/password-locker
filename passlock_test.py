c5b2771a069168353ff84de369278adbb43ef72b
first-name second-name <Samuel2003ngash@gmail.com>
2022-02-21 11:25:28 +0300

    first commit

--- passlock_test.py
+++ passlock_test.py
@@ -10,7 +10,7 @@ class TestClass(unittest.TestCase):
         """
         Method that runs before each individual test methods run.
         """
-        self.new_user = User('SamuelNdienye','XyZ3thf1')
+        self.new_user = User('SamuelNdienye','hf1')
 
     def test_init(self):
         """
